77043a9d1bb58f8378de2ced7dd10fba3c9b5e7b
BraedenOlney <129467921+BraedenOlney@users.noreply.github.com>
2023-05-22 21:44:44 -0700

    Add files via upload
    Controllers

---
+++ AdminLoginController.java
@@ -0,0 +1,22 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+//import javafx.scene.control.*;
+
+public class AdminLoginController {
+	@FXML
+	private Label AdminPasswordLabel;
+	@FXML
+	private Label AdminUsernameLabel;
+	@FXML
+	private TextField AdminUsernameText;
+	@FXML
+	private TextField AdminPasswordText;
+	//@FXML
+	//private ImageView ABSPic;
+	@FXML
+	private Button AdminLoginButton;
+}
---
+++ UserLoginController.java
@@ -0,0 +1,22 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+//import javafx.scene.control.*;
+
+public class UserLoginController {
+	@FXML
+	private Label CustomerPasswordLabel;
+	@FXML
+	private Label CustomerUsernameLabel;
+	@FXML
+	private TextField CustomerUsernameText;
+	@FXML
+	private TextField CustomerPasswordText;
+	//@FXML
+	//private ImageView ABSPic;
+	@FXML
+	private Button CustomerLoginButton;
+}
