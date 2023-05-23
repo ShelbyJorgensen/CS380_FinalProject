dfed91633951986fc62a346052c9bab876f88e7b
Shelby Jorgensen <126823300+ShelbyJorgensen@users.noreply.github.com>
2023-05-23 12:50:45 -0700

    Update UserLoginController.java

--- UserLoginController.java
+++ UserLoginController.java
@@ -1,38 +1,54 @@
 package application;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
-import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 public class UserLoginController {
+	
+	@FXML
+	private TextField CustomerUsernameText;
+	
+	public void UserLogin(ActionEvent event) throws Exception {
+		Connection connection = null;
+		
+		try {
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/finalProject", "root", "");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	    PreparedStatement p = null;
+	    ResultSet rs = null;
 
-    @FXML
-    private ImageView ABSPic;
-
-    @FXML
-    private Button CustomerLoginButton;
-
-    @FXML
-    private Label CustomerLoginLabel;
-
-    @FXML
-    private Label CustomerPasswordLabel;
-
-    @FXML
-    private TextField CustomerPasswordText;
-
-    @FXML
-    private Label CustomerUsernameLabel;
-
-    @FXML
-    private TextField CustomerUsernameText;
-
-    @FXML
-    void Login(ActionEvent event) {
-
-    }
-
+	    String sql = "SELECT * FROM users";
+	    p = connection.prepareStatement(sql);
+	    rs = p.executeQuery();
+	    
+	    boolean isValid = false;
+	    // Check database for valid username
+	    while (rs.next()) {
+	        String name = rs.getString("username");
+	        if(name.equals(CustomerUsernameText.getText())) {
+	        	isValid = true;
+	        }
+	    }
+		
+	    if(isValid) {
+			Stage primaryStage = new Stage();
+			Parent root = FXMLLoader.load(getClass().getResource("/application/User.fxml"));
+			Scene scene = new Scene(root, 700, 500);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+	    }
+	}
 }
