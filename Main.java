c11487c42788805dec6bf4477862c05a30d45345
Shelby Jorgensen <126823300+ShelbyJorgensen@users.noreply.github.com>
2023-05-22 22:41:41 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,38 @@
-package finalProject;
+package application;
 
-public class Main {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Control;
+//import javafx.scene.layout.BorderPane;
+
+public class Main extends Application {
+	
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/dbconnector", "root", "AspenBoots22");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			//BorderPane root = new BorderPane();
+			Parent root = FXMLLoader.load(getClass().getResource("/application/Home.fxml"));
+			Scene scene = new Scene(root, 400, 400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
