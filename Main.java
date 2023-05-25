95e2eb9c0e0199f345927c6c82bdcecad6331857
Shelby Jorgensen <126823300+ShelbyJorgensen@users.noreply.github.com>
2023-05-25 11:50:05 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,15 @@
-package finalProject;
+package application;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.stage.Stage;
+import javafx.scene.control.Control;
+//import javafx.scene.layout.BorderPane;
 
 public class Main extends Application {
 	
@@ -13,7 +17,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			//BorderPane root = new BorderPane();
-			Parent root = FXMLLoader.load(getClass().getResource("/finalProject/Home.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("/application/Home.fxml"));
 			Scene scene = new Scene(root, 700, 500);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
